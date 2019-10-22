009def672842afcde57e8aae271db0287da11e9a
riva <rivayudha@gmail.com>
2019-10-23 00:25:38 +0700

    Sikit lagi.

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,10 +59,7 @@ module.exports = {
 				icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
 			},
 		},
-		{
-			resolve: `gatsby-plugin-styled-components`,
-			options: {},
-		},
+		`gatsby-plugin-styled-components`,
 		{
 			resolve: `gatsby-source-contentful`,
 			options: {
